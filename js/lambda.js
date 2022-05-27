7ecf5091f03fe819b3fe0cc75cd6fa380efe9ee2
mgh <mattghall@live.com>
2022-05-26 20:58:18 -0700

    Use asyncs and awaits more correctly

--- js/lambda.js
+++ js/lambda.js
@@ -9,34 +9,76 @@ var url = "https://" + HOST + SCHEDULE_URL;
 var seasonTotals = new Map();
 var schedule = new Map();
 
-
-exports.handler = function(event, context, callback) {
-  downloadSchedule(event, callback);
+exports.handler = async function(event, context, callback) {
+  console.log("first");
+  await downloadSchedule(event, callback);
+  console.log("second");
+  await processMethod(event, callback);
+  console.log("third");
 };
 
-function processMethod(event) {
+function processMethod(event, callback) {
   var method = event.method;
-  if (method == "process") {
-    var date = event.date;
-    console.log("Processing games for " + date);
-    getDataForDate(date);
-  } else if (method == "backfill") {
-    console.log("Backfilling missing data");
+  return new Promise(resolve => {
+     if (method == "process") {
+      var date = event.date;
+      console.log("Processing games for " + date);
+      getDataForDate(date, callback);
+    } else if (method == "backfill") {
+      console.log("Backfilling missing data");
+    }
+  });
+}
+
+async function downloadSchedule(event, callback) {
+  if (schedule.size > 0) {
+    console.log("Schedule already downloaded");
+  } else {
+    return new Promise(resolve => {
+      scheduleHttpOptions();
+      console.log('Getting schedule — start request to ' + url);
+      var result = '';
+
+      https.get(httpOptions, (response) => {
+        console.log("resp: " + response.statusCode);
+
+        response.on('data', function(responseData) {
+          result += responseData;
+        });
+        response.on('error', function(responseData) {
+          result += "error";
+          console.log("error: " + responseData);
+        });
+
+        response.on('end', function() {
+          if (result != 'error') {
+            schedule = new Map();
+            JSON.parse(result).dates.forEach(object => {
+              schedule.set(object.date, object);
+            });
+            console.log("Schedule set");
+            resolve();
+          } else {
+            console.log("error");
+            return;
+          }
+        });
+      });
+    });
   }
 }
 
-
-
-function downloadSchedule(event, callback) {
-  if (schedule.size > 0) {
-    console.log("Schedule already downloaded");
-    processMethod(event);
+async function getBoxScore(gamePk) {
+  if (seasonTotals.has(gamePk)) {
+    console.log("Game " + gamePk + " already processed");
+    return printTweet(seasonTotals.get(gamePk));
   } else {
-    makeHttpOptions();
+    console.log("Getting Box Score Data for " + gamePk);
+    boxHttpOptions(gamePk);
     console.log('start request to ' + url);
     var result = '';
 
-    https.get(httpOptions, (response) => {
+    await https.get(httpOptions, (response) => {
       console.log("resp: " + response.statusCode);
       response.on('data', function(responseData) {
         result += responseData;
@@ -48,15 +90,9 @@ function downloadSchedule(event, callback) {
 
       response.on('end', function() {
         if (result != 'error') {
-          schedule = new Map();
-          JSON.parse(result).dates.forEach(object => {
-            schedule.set(object.date, object);
-          });
-          console.log("Schedule set");
-          processMethod(event);
+          return processBox(JSON.parse(result));
         } else {
           console.log("error");
-          callback(null, buildResp(500, "error"));
           return;
         }
       });
@@ -65,145 +101,107 @@ function downloadSchedule(event, callback) {
 }
 
 
-
-function buildResp(code, body, usingCache) {
-  if (code == 200) {
-    console.log(body);
-  } else {
-    console.log("error");
-  }
-}
-
-function makeHeaders(usingCache) {
-  return {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-    "Access-Control-Allow-Headers": "force-update",
-    "Using-Cache": usingCache + ""
-  };
-}
-
-
-function getBoxScore(gamePk) {
-  if (seasonTotals.has(gamePk)) {
-    return printTweet(seasonTotals.get(gamePk));
-  } else {
-    console.log("Getting Box Score Data for " + gamePk);
-    var url = "https://statsapi.mlb.com/api/v1.1/game/" + gamePk + "/feed/live?language=en";
-
-    // fetch(url, {})
-    //   .then(function(response) {
-    //     response.text().then(function(text) {
-    //       processBox(JSON.parse(text));
-    //     });
-    //   })
-    //   .catch(function(err) {
-    //     console.log("Error: " + err);
-    //     window.location.href = "error.html"
-    //   });
-  }
-}
-
-function getDataForDate(date) {
+async function getDataForDate(date, callback) {
   if (!schedule.has(date)) {
     console.log("No games played on " + date);
   } else {
     var gamePks = [];
     var data = schedule.get(date).games;
-    for (game of data) {
+    for (var game of data) {
       gamePks.push(game.gamePk);
     }
+    console.log("Processing games " + gamePks);
     var i = 0;
-    for (gamePk of gamePks) {
+    var boxScores = [];
+    for (var gamePk of gamePks) {
       var msSleep = i * 250;
       if (i > 0) {
-        console.log("waiting " + msSleep + "ms to process " + gamePk)
+        console.log("waiting " + msSleep + "ms to process " + gamePk);
       }
-      setTimeout(() => {
-        getBoxScore(gamePk);
+      await setTimeout(() => {
+        boxScores.push(getBoxScore(gamePk, callback));
       }, msSleep);
       i++;
     }
+    return boxScores;
   }
 }
-//
-// function processBox(boxScore) {
-//   var loc = "home";
-//   var vsAt = "vs";
-//   var opponent = boxScore.gameData.teams.away.abbreviation;
-//   if (opponent == "SEA") {
-//     opponent = boxScore.gameData.teams.home.abbreviation;
-//     vsAt = "at";
-//     loc = "away";
-//   }
-//   var gameDateTime = new Date(boxScore.gameData.datetime.dateTime);
-//   var date = (gameDateTime.getMonth() + 1) + "/" + gameDateTime.getDate();
-//
-//   if (boxScore.gameData.status.detailedState != "Final") {
-//     if (boxScore.gameData.status.detailedState == "Scheduled") {
-//       console.log("Game has not started");
-//     } else {
-//       console.log("Game has not ended");
-//     }
-//   } else {
-//     var info = boxScore.liveData.boxscore.teams[loc].info[0].fieldList;
-//     info = info.reduce(function(map, obj) {
-//       map[obj.label] = obj;
-//       return map;
-//     }, {});
-//
-//     if (info.hasOwnProperty("Runners left in scoring position, 2 out")) {
-//       var rispRaw = info["Runners left in scoring position, 2 out"].value.replace(".", "");
-//       var runners = rispRaw.replace(".", "").split(";")
-//     } else {
-//       var rispRaw = "Rest Day!";
-//       var runners = [];
-//     }
-//
-//     var gameTotal = 0;
-//     for (runner of runners) {
-//       var qty = runner.match(/\d+/);
-//       if (qty == null) {
-//         gameTotal++;
-//       } else {
-//         gameTotal += parseInt(qty[0])
-//       }
-//     }
-//
-//     var tweet = {
-//       gameTotal: gameTotal,
-//       header: "#Miles4RISP " + vsAt + " " + opponent + " " + date,
-//       batters: rispRaw,
-//       seasonTotal: addSeasonTotal(gameTotal)
-//     };
-//     seasonTotals.set(boxScore.gameData.game.pk, tweet);
-//     printTweet(tweet);
-//   }
-// }
-//
+
+async function processBox(boxScore, callback) {
+  console.log("Processing Box " + boxScore.gameData.game.pk);
+  var loc = "home";
+  var vsAt = "vs";
+  var opponent = boxScore.gameData.teams.away.abbreviation;
+  if (opponent == "SEA") {
+    opponent = boxScore.gameData.teams.home.abbreviation;
+    vsAt = "at";
+    loc = "away";
+  }
+  var gameDateTime = new Date(boxScore.gameData.datetime.dateTime);
+  var date = (gameDateTime.getMonth() + 1) + "/" + gameDateTime.getDate();
+
+  if (boxScore.gameData.status.detailedState != "Final") {
+    if (boxScore.gameData.status.detailedState == "Scheduled") {
+      console.log("Game has not started");
+      return "Game has not started";
+    } else {
+      console.log("Game has not ended");
+      return "Game has not ended";
+    }
+  } else {
+    console.log("processing completed game");
+    var info = boxScore.liveData.boxscore.teams[loc].info[0].fieldList;
+    info = info.reduce(function(map, obj) {
+      map[obj.label] = obj;
+      return map;
+    }, {});
+    var rispRaw = "Runners left in scoring position, 2 out";
+    var runners = [];
+    if (info.hasOwnProperty("Runners left in scoring position, 2 out")) {
+      rispRaw = info["Runners left in scoring position, 2 out"].value.replace(".", "");
+      runners = rispRaw.replace(".", "").split(";");
+    } else {
+      rispRaw = "Rest Day!";
+    }
+
+    var gameTotal = 0;
+    for (var runner of runners) {
+      var qty = runner.match(/\d+/);
+      if (qty == null) {
+        gameTotal++;
+      } else {
+        gameTotal += parseInt(qty[0]);
+      }
+    }
+
+    var tweet = {
+      gameTotal: gameTotal,
+      header: "#Miles4RISP " + vsAt + " " + opponent + " " + date,
+      batters: runners,
+      battersMsg: rispRaw,
+      seasonTotal: addSeasonTotal(gameTotal)
+    };
+    console.log("tweet:\n" + printTweet(tweet));
+    seasonTotals.set(boxScore.gameData.game.pk, tweet);
+    return tweet;
+    // callback(null, tweet);
+  }
+}
+
+
 function printTweet(tweet) {
   var msg = tweet.gameTotal + " " + tweet.header + "\n" +
-    tweet.batters + "\n" +
+    tweet.battersMsg + "\n" +
     "Season Total: " + tweet.seasonTotal;
   return msg;
-
 }
-//
-// function addSeasonTotal(gameTotal) {
-//   return gameTotal + Array.from(seasonTotals.values()).reduce(function(tot, arr) {
-//     return tot + arr.gameTotal;
-//   }, 0);
-// }
-//
-// function getDaysArray(start, end) {
-//   var arr = [];
-//   for (dt = new Date(start); dt <= new Date(end); dt.setDate(dt.getDate() + 1)) {
-//     arr.push((new Date(dt)).toISOString().split('T')[0]);
-//   }
-//   return arr;
-// };
-//
-//
+
+function addSeasonTotal(gameTotal) {
+  return gameTotal + Array.from(seasonTotals.values()).reduce(function(tot, arr) {
+    return tot + arr.gameTotal;
+  }, 0);
+}
+
 // var backFillQueue = [];
 //
 // function backFillMe() {
@@ -238,10 +236,21 @@ function printTweet(tweet) {
 //   return previous;
 // }
 
-function makeHttpOptions() {
+function scheduleHttpOptions() {
   httpOptions = {
     hostname: HOST,
     path: SCHEDULE_URL,
+    headers: {
+      "Accept": "application/json"
+    }
+  };
+}
+
+function boxHttpOptions(gamePk) {
+  var url = "/api/v1.1/game/" + gamePk + "/feed/live?language=en";
+  httpOptions = {
+    hostname: HOST,
+    path: url,
     headers: {
       "Accept": "application/json"
     }
